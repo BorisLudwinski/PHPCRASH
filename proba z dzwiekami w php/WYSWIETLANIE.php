c4bd3f9852fb0e9e8c56759b8e1d81c96c891ee0
BorisLudwinski <boris.ludwinski@gmail.com>
2022-09-18 17:47:38 +0200

    kombinatoryka (dzwieki na gryfie)

--- proba z dzwiekami w php/WYSWIETLANIE.php
+++ proba z dzwiekami w php/WYSWIETLANIE.php
@@ -1,16 +1,18 @@
 <?php
-$dzwieki = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
+$matrix = [];
 
-$pierwszy_dzwiek = array_shift($dzwieki);
-$dzwieki1 = array_push($dzwieki, $pierwszy_dzwiek);
-
- print_r($pierwszy_dzwiek);
- print_r($dzwieki);
- 
-//  print_r ($new_numbers);
+$dzwieki[] = ["c", "c#", "d", "d#", "e", "f", "f#", "g", "g#", "a", "a#", "b"];
 
 
-//  for($x = 0; $x <= 11; $x++) {
-//     echo " number: $x<br/>";
-// }
+
+for ($x=0; $x < count($dzwieki); $x++) {
+    $matrix[$x] = array_push($dzwieki[$x], array_shift($dzwieki[$x]));
+
+
+}
+//  print_r($dzwieki);
+ print_r($matrix);
+
+
+
 ?>
